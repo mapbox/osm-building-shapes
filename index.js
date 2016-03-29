acd92054d90cda0afea88fcf94edce254281d071
Oindrila Gupta <oindrila@mapbox.com>
2016-03-29 18:09:40 +0530

    Calculate area and perimeter for each polygon

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var turf = require('turf');
 
 var out = turf.featurecollection([]);
 tileReduce({
-  bbox:[77.5079, 12.9042, 77.7049, 13.0507],
+  bbox:[77.6260, 12.9610, 77.6624, 12.9872],
   zoom: 12,
   map: path.join(__dirname, 'map.js'),
   sources: [{
@@ -14,10 +14,6 @@ tileReduce({
     raw: false
   }]
 })
-.on('reduce', function(data) {
-  out.features = out.features.concat(data.features);
-})
-.on('end', function() {
-  console.log(JSON.stringify(out));
-});
+.on('reduce', function() {})
+.on('end', function() {});
 
--- map.js
+++ map.js
@@ -3,16 +3,23 @@ var turf = require('turf');
 
 // Identify Point geometries with a boundary tag.
 module.exports = function(tileLayers, tile, writeData, done) {
-  var layer = tileLayers.osm.osm;
+	var layer = tileLayers.osm.osm;
 
-  var buildings = layer.features.filter(function(val) {
-    if (val.properties.building && val.geometry.type === 'Polygon') {
-      return true;
-    }
-  });
+	var buildings = layer.features.filter(function(val) {
+		if (val.properties.building && val.geometry.type === 'Polygon') {
+			var coordinates_length = val.geometry.coordinates.length;
+			if (val.geometry.coordinates[0] === val.geometry.coordinates[coordinates_length - 1]) {
+				val.properties.calc_area = turf.area(val);
+				val.properties.calc_perimeter = turf.lineDistance(val);
+				return true;
+			}
+		}
+	});
 
-  var fc = turf.featurecollection(buildings);
+	if (buildings.length > 0) {
+		var fc = turf.featurecollection(buildings);
+		writeData(JSON.stringify(fc) + '\n');
+	}
 
-  done(null, fc);
+	done(null, null);
 };
-
