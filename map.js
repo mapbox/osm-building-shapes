a974c6872cfc3fa846a960e5178293c5e2c024ae
maning <emmanuel.sambale@gmail.com>
2016-03-30 11:53:09 +0530

    better comments on the indices

--- map.js
+++ map.js
@@ -16,11 +16,13 @@ module.exports = function(tileLayers, tile, writeData, done) {
 		            props.area = turf.area(val);
                             //perimeter in meter
 			    props.perimeter = (turf.lineDistance(val,'kilometers')/0.001);
-			    //perimeter area ratio https://spatialanalysisonline.com
+			    //para = (perimeter / area )
 			    props.para = ((props.perimeter)/(props.area));
-			    //p2a https://spatialanalysisonline.com
+			    //p2a = (perimeter^2) / (area) http://www.spatialanalysisonline.com/HTML/index.html?shape.htm
 			    props.p2a = ((props.perimeter)*(props.perimeter))/(props.area);
 			    props.sqrt_p2a = Math.sqrt(props.p2a);
+			    // shape factor = (4 * PI * area) / (perimeter^2) http://www.empix.com/NE%20HELP/functions/glossary/morphometric_param.htm
+			    props.shape = (Math.PI * 4 * props.area) / ((props.perimeter)*(props.perimeter));
 			    val.properties = props
 				return true;
 			}
