e661ecebe4aa917a05c71dd121767c4e89819d60
Sajjad Anwar <me@sajjad.in>
2016-04-05 17:52:42 +0530

    cleanup. use within and points to check tile edge

--- index.js
+++ index.js
@@ -9,21 +9,15 @@ var file = argv.file;
 var bbox = JSON.parse(argv.bbox);
 var zoom = argv.zoom;
 
-
-
-  tileReduce({
+tileReduce({
     bbox: bbox,
     zoom: zoom,
     map: path.join(__dirname, '/map.js'),
     sources: [{
-      name: 'osm',
-      mbtiles: path.join(__dirname, file),
-      raw: false
+        name: 'osm',
+        mbtiles: path.join(__dirname, file),
+        raw: false
     }]
-  })
-  .on('reduce', function() {})
-  .on('end', function() {
-
-  });
-
-
+})
+.on('reduce', function() {})
+.on('end', function() {});
--- map.js
+++ map.js
@@ -4,46 +4,43 @@ var tilebelt = require('tilebelt');
 
 
 module.exports = function(tileLayers, tile, writeData, done) {
-	var layer = tileLayers.osm.osm;
-	var bbox = tilebelt.tileToBBOX(tile);
-	var bboxLineString = turf.bboxPolygon(bbox);
-	bboxLineString.geometry.type = 'LineString';
-	bboxLineString.geometry.coordinates = bboxLineString.geometry.coordinates[0];
-	var buffer = turf.buffer(bboxLineString, 5, 'meters').features[0];
+    var layer = tileLayers.osm.osm;
+    var bbox = tilebelt.tileToBBOX(tile);
+    var bboxLineString = turf.bboxPolygon(bbox);
+    bboxLineString.geometry.type = 'LineString';
+    bboxLineString.geometry.coordinates = bboxLineString.geometry.coordinates[0];
+    var buffer = turf.featurecollection(turf.buffer(bboxLineString, 5, 'meters').features[0]);
 
-	var buildings = layer.features.filter(function(val) {
-		if (val.properties.building && val.geometry.type === 'Polygon') {
-			var flag = 0;
-			for (var i = 0; i < val.geometry.coordinates[0].length; i++) {
-				if (turf.inside(turf.point(val.geometry.coordinates[0][i]), buffer)) {
-					flag = 1;
-					break;
-				}
-			}
-			if (flag == 0) {
-				var props = {
-				    "_osm_way_id": val.properties._osm_way_id,
-				    "building": val.properties.building,
-				};
-			    //area in m^2
-			    props.area = parseFloat((turf.area(val)).toFixed(3));
-			    
-			    //perimeter in meter
-			    props.perimeter = parseFloat(((turf.lineDistance(val,'kilometers') * 1000 )).toFixed(3));
-			    
-			    // shape factor = (4 * PI * area) / (perimeter^2) http://www.empix.com/NE%20HELP/functions/glossary/morphometric_param.htm
+    var buildings = layer.features.filter(function(val) {
+        if (val.properties.building && val.geometry.type === 'Polygon') {
+            var flag = 0;
+            var buildingPoints = turf.explode(val);
+            var pointsWithin = turf.within(buildingPoints, buffer);
+            if (!pointsWithin.features.length) {
+                var props = {
+                    "_osm_way_id": val.properties._osm_way_id,
+                    "building": val.properties.building,
+                };
 
-			    props.shape = parseFloat(((((Math.PI * 4 * props.area) / ((props.perimeter)*(props.perimeter))) * 100)).toFixed(3));
-			    val.properties = props
-			    return true;
-			}
-		}
-	});
+                //area in m^2
+                props.area = parseFloat((turf.area(val)).toFixed(3));
 
-	if (buildings.length > 0) {
-		var fc = turf.featurecollection(buildings);
-		writeData(JSON.stringify(fc) + '\n');
-	}
+                //perimeter in meter
+                props.perimeter = parseFloat(((turf.lineDistance(val,'kilometers') * 1000 )).toFixed(3));
 
-	done(null, null);
+                // shape factor = (4 * PI * area) / (perimeter^2) http://www.empix.com/NE%20HELP/functions/glossary/morphometric_param.htm
+
+                props.shape = parseFloat(((((Math.PI * 4 * props.area) / ((props.perimeter)*(props.perimeter))) * 100)).toFixed(3));
+                val.properties = props;
+                return true;
+            }
+        }
+    });
+
+    if (buildings.length > 0) {
+        var fc = turf.featurecollection(buildings);
+        writeData(JSON.stringify(fc) + '\n');
+    }
+
+    done(null, null);
 };
